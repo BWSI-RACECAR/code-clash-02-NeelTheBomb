1dde7792baf1dbde80d918b612984c195812cbcb
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-07-13 09:22:36 -0400

    Update digitdecrypt.py

--- digitdecrypt.py
+++ digitdecrypt.py
@@ -7,9 +7,9 @@ class Solution:
             num1 = 0
             while len(strin) > 1:
                 num1 = 0
-                    for i in strin:
-                         num1 = num1 + int(strin)
-                    strin = str(num1)
+                for i in strin:
+                    num1 = num1 + int(strin)
+                strin = str(num1)
             return num1
             
             #TODO: Write code below to returnn an int with the solution to the prompt.
