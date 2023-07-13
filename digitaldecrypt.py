7bf77fa747272478abfd2f957429d48cce989b98
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-07-13 09:03:37 -0400

    Add files via upload

---
+++ digitaldecrypt.py
@@ -0,0 +1,23 @@
+class Solution:    
+    def digitdecrypt(self, num):
+            #type num: int
+            #return type: int
+            strin = str(num)
+            for i in range(len(strin)):
+                 num = num + int(string[i])
+            return num
+            
+            #TODO: Write code below to returnn an int with the solution to the prompt.
+            pass
+ 
+def main():
+    input1= input()
+    input2 = int(input1)
+
+    
+    tc1 = Solution()
+    ans = tc1.digitdecrypt(input2)
+    print(ans)
+    
+if __name__ == "__main__":
+    main()
